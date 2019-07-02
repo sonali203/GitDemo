c7c590df2e3b6c7811f0121ddd3f3c62717f55d1
sonali203 <sonasrivastav22@gmail.com>
2019-07-03 00:40:46 +0530

    first commit

---
+++ src/test/java/SampleMaven/MaveTask/DTest.java
@@ -0,0 +1,16 @@
+package SampleMaven.MaveTask;
+
+import org.testng.annotations.Test;
+
+public class DTest {
+	@Test
+	public void Seven()
+	{
+	System.out.println("Seven");	
+	}
+	@Test
+	public void Eight()
+	{
+	System.out.println("Eight");	
+	}
+}
---
+++ src/test/java/SampleMaven/MaveTask/ETest.java
@@ -0,0 +1,16 @@
+package SampleMaven.MaveTask;
+
+import org.testng.annotations.Test;
+
+public class ETest {
+	@Test
+	public void Nine()
+	{
+	System.out.println("Nine");	
+	}
+	@Test
+	public void Ten()
+	{
+	System.out.println("Ten");	
+	}
+}
